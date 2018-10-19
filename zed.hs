e961362ed2aaeec9642e199bdb212e85d0d698c4
franarama <franramunno@gmail.com>
2018-10-18 17:48:35 -0700

    Add check for unique rows/cols

--- zed.hs
+++ zed.hs
@@ -1,6 +1,5 @@
 import Data.List
 
-
 -- Returns a list of all possible maps with side length n.
 -- Essentially returns n combinations of n permutations.
 getMaps :: Int -> [[[Int]]]
@@ -11,4 +10,15 @@ getMaps n = combinations (permutations [1..n]) []
       | n == length acc = [acc]
       | otherwise = combinations xs (x:acc) ++ combinations xs acc
 
+-- Helper function that simply checks if a list has all unique elements.
+isUnique :: Eq a => [a] -> Bool
+isUnique [] = True
+isUnique (x:xs)
+  | x `elem` xs = False
+  | otherwise = isUnique xs
+
+-- Checks the rows and columns for uniqueness for a given map.
+hasUniqueRowsAndCols :: Eq a => [[a]] -> Bool
+hasUniqueRowsAndCols kzMap = and (map isUnique kzMap) &&
+                             and (map isUnique (transpose kzMap))
 
